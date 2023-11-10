a51146d1a93539144500e7f01bea30f330cf1e00
farrell-dan <140118556+farrell-dan@users.noreply.github.com>
2023-11-10 11:51:32 -0500

    Exercise 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
-import GlobalStyles from './GlobalStyles';
+import { useContext, useEffect } from "react";
+import GlobalStyles from "./GlobalStyles";
+import { SeatContext } from "./SeatContext";
 
 const App = () => {
+  const {
+    state: { numOfRows },
+    actions: { receiveSeatInfoFromServer },
+  } = useContext(SeatContext);
+
+  useEffect(() => {
+    fetch("/api/seat-availability")
+      .then((res) => res.json())
+      .then((data) => receiveSeatInfoFromServer(data));
+  }, []);
+
   return (
     <>
       <GlobalStyles />
-      TODO: write code
+      This vanue has {numOfRows} rows!
     </>
   );
-}
+};
 
 export default App;
--- src/components/SeatContext.js
+++ src/components/SeatContext.js
@@ -3,37 +3,48 @@ import { createContext, useReducer } from "react";
 export const SeatContext = createContext();
 
 const initialState = {
-    hasLoaded: false,
-    seats: null,
-    numOfRows: 0,
-    seatsPerRow: 0,
+  hasLoaded: false,
+  seats: null,
+  numOfRows: 0,
+  seatsPerRow: 0,
 };
 
-const recuder = (state, action) => {
-    // TODO
+const reducer = (state, action) => {
+switch (action.type) {
+    case 'receive-seat-info-from-server':{
+        return {
+            ...state,
+            hasLoaded: true,
+            seats: action.seats ,
+            numOfRows: action.numOfRows,
+            seatsPerRow: action.seatsPerRow,
+        }
+    }
+    default:
+        throw new Error (`Unrecognized action: ${action.type}`)
 }
-
-export const SeatProvider = ({children}) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
-
-    const receiveSeatInfoFromServer = (data) => {
-        dispatch ({
-            type: "receive-seat-info-from-server",
-            ...data,
-        })
-    };
-
-    return (
-        <SeatContext.Provider
-            value = {{
-                state,
-                actions: {
-                    receiveSeatInfoFromServer,
-                },
-            }}
-        >
-            {children}
-        </SeatContext.Provider>
-    );
 };
 
+export const SeatProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const receiveSeatInfoFromServer = (data) => {
+    dispatch({
+      type: "receive-seat-info-from-server",
+      ...data,
+    });
+  };
+
+  return (
+    <SeatContext.Provider
+      value={{
+        state,
+        actions: {
+          receiveSeatInfoFromServer,
+        },
+      }}
+    >
+      {children}
+    </SeatContext.Provider>
+  );
+};
